1c0533ef8c4a7e0bdc107ec91a6e25eeab25f9ec
Art9996 <apresyanartur16@gmail.com>
2024-04-07 19:32:46 +0400

    my second work

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('hello')
+const express =require('express');
+const app =express();
+const path = require('path')
+const http = require('http')
+const socket = require('socket.io')
+const PORT = 3001
+
+const server = http.createServer(app)
+server.listen(PORT,()=>{
+console.log(`server is running on port${PORT} `);
+})
+
+app.use(express.static(path.join(__dirname,
+  "public")))
+
+  const io=socket(server)
+  io.on("connection",(socket)=>{
+    console.log('User is connected')
+  })
+
+
+
